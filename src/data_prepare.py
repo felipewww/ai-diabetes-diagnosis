e074c608d1edc01655cd01d91e0a21a56825120a
Felipe Barreiros <felipewww@outlook.com.br>
2025-07-28 16:31:19 -0300

    boost some cols, cross_validation_all metrics

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def zero_to_mean(df: pd.DataFrame):
     non_zero_cols = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
+
     df[non_zero_cols] = df[non_zero_cols].replace(0, np.nan)
 
     for col in non_zero_cols:
